bbc42d5b956896bd50b777be525d99f45137e05b
constfps <constfps@gmail.com>
2025-08-27 13:06:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Shit left to do
+---
+- [ ] Wofi
+- [ ] Notification Daemon
+- [ ] Modulate Hyprland config
+- [ ] Finalize Hyprland binds
+- [ ] Customize animations
+- [ ] Auth agent
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,60 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "nvim-java/nvim-java"
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "clangd",
+                    "cssls",
+                    "jdtls",
+                    "pylsp",
+                    "bashls",
+                    "intelephense"
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            lspconfig.cssls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.jdtls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pylsp.setup({
+                capabilities = capabilities
+            })
+            lspconfig.bashls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.intelephense.setup({
+                capabilities = capabilities
+            })
+        end
+    }
+}
